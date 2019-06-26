8f1913c3cf4c950c1319b987fa559221feafa777
Jon Cheung <jacheung6@gmail.com>
2019-06-26 16:01:01 -0700

    single dimension hilbert touch tuning complete

--- matlab/code/sandbox/multi_hilbert_touch.m
+++ matlab/code/sandbox/multi_hilbert_touch.m
@@ -25,6 +25,15 @@ for rec = 1:length(U)
 end
 
 %% 1 dimensional hilbert features
+
+%% Quantifying object location tuning
+touchCells = touchCell(U,'off');
+selectedCells = find(touchCells==1);
+whichTouches = fields(popV{1});
+fieldsList = fields(popV{1}.allTouches);
+hilbertTouch = tuningQuantification(U,popV,selectedCells,fieldsList([ 1 3 4 5]),whichTouches,touchWindow,'off');
+
+
 [rc]= numSubplots(length(U))
 
 fieldsToCompare = fields(hilbertTouch.R_ntk.allTouches);
@@ -34,55 +43,70 @@ peakResponse = cell(1,numel(fieldsToCompare));
 
 pctSamp = .01; 
 smoothParam = .7;
-for i = 1:4
+alpha = .01; 
+for i = 4
     figure(230);clf
-    featureMean = cellfun(@(x) nanmean(x),hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}),'uniformoutput',0);
-    featureSEM = cellfun(@(x) nanstd(x) ./ sqrt(sum(~isnan(x))),hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}),'uniformoutput',0);
-    sampleSize = cellfun(@(x) sum(~isnan(x)),hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}),'uniformoutput',0);
+    featureMean = cellfun(@(x) nanmean(x,2),hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}),'uniformoutput',0);
+    featureSEM = cellfun(@(x) nanstd(x,[],2) ./ sqrt(sum(~isnan(x),2)),hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}),'uniformoutput',0);
+    sampleSize = cellfun(@(x) sum(~isnan(x),2),hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}),'uniformoutput',0);
     tscore = cellfun(@(x) tinv(.0975,x-1), sampleSize,'uniformoutput',0);
     CI = cellfun(@(x,y) x.*y,featureSEM,tscore,'uniformoutput',0);
     
 
-    selBins= cellfun(@(x) sum(~isnan(x))> round(sum(~isnan(x(:)))*pctSamp),hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}),'uniformoutput',0);
+    selBins= cellfun(@(x) sum(~isnan(x),2)> round(sum(~isnan(x(:)))*pctSamp),hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}),'uniformoutput',0);
     x = hilbertTouch.S_ctk.(fieldsToCompare{i});
     for d = 1:length(U)
-        [THilbertP{i}(d),~,~] = anova1(hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}){d},[],'off');
-
-        subplot(rc(1),rc(2),d);
-        if strcmp(fieldsToCompare{i},'phase')
-            f = fit((1:numel(x))',(featureMean{d}.*1000)','smoothingspline','SmoothingParam',smoothParam);
-            fCI = fit((1:numel(x))',(CI{d}.*1000)','smoothingspline','SmoothingParam',smoothParam);
-            fitline = f(1:numel(x));
-            fCIline = fCI(1:numel(x)); 
+        if ~isempty(featureMean{d})
             
-            bar(x,featureMean{d}.*1000,'facecolor',[.8 .8 .8])
-            hold on; shadedErrorBar(x,fitline,fCIline,'g')
-            set(gca,'xtick',-pi:pi:pi,'xticklabel',{'-\pi','0','\pi'})
+            [THilbertP{i}(d),~,~] = anova1(hilbertTouch.R_ntk.allTouches.(fieldsToCompare{i}){d}',[],'off');
             
-            [~,idx] = max(fitline);
-            peakResponse{i}(d) = x(idx); 
-        else
-            xtv = x(selBins{d}); 
-            
-            bar(xtv,featureMean{d}(selBins{d}).*1000,'facecolor',[.8 .8 .8])
-            f = fit(xtv',(featureMean{d}(selBins{d}).*1000)','smoothingspline','SmoothingParam',smoothParam);
-            fCI = fit(xtv',(CI{d}(selBins{d}).*1000)','smoothingspline','SmoothingParam',smoothParam);
-            fitline = f(xtv(1):xtv(end));
-            fCIline = fCI(xtv(1):xtv(end)); 
-            hold on; shadedErrorBar(xtv(1):xtv(end),fitline,fCIline,'g')
-           
-            set(gca,'xtick',xtv(1):3:xtv(end),'ylim',[0 (max(featureMean{d}(selBins{d}).*1000)+.01).*1.2])
-            
-            [~,idx] = max(fitline);
-            ranges = xtv(1):xtv(end);
-            peakResponse{i}(d) = ranges(idx); 
+            subplot(rc(1),rc(2),d);
+            if strcmp(fieldsToCompare{i},'phase')
+                x = linspace(-pi,pi,12);
+                xtv = x(selBins{d})';
+                
+                %bar(x,featureMean{d}.*1000,'facecolor',[.8 .8 .8])
+                f = fit(xtv,(featureMean{d}(selBins{d}).*1000),'smoothingspline','SmoothingParam',smoothParam);
+                fCI = fit(xtv,(CI{d}(selBins{d}).*1000),'smoothingspline','SmoothingParam',smoothParam);
+                fitline = f(x);
+                fCIline = fCI(x);
+                
+                if THilbertP{i}(d)< alpha
+                    hold on; shadedErrorBar(x,fitline,fCIline,'g')
+                else
+                    hold on; shadedErrorBar(x,fitline,fCIline,'k')
+                end
+                
+                set(gca,'xlim',[-pi pi],'xtick',-pi:pi:pi,'xticklabel',{'-\pi','0','\pi'})
+                [~,idx] = max(fitline);
+                peakResponse{i}(d) = x(idx);
+            else
+                xtv = x(selBins{d})';
+                
+%               bar(xtv,featureMean{d}(selBins{d}).*1000,'facecolor',[.8 .8 .8])
+                f = fit(xtv,(featureMean{d}(selBins{d}).*1000),'smoothingspline','SmoothingParam',smoothParam);
+                fCI = fit(xtv,(CI{d}(selBins{d}).*1000),'smoothingspline','SmoothingParam',smoothParam);
+                fitline = f(xtv(1):xtv(end));
+                fCIline = fCI(xtv(1):xtv(end));
+                
+                if THilbertP{i}(d)< alpha
+                    hold on; shadedErrorBar(xtv(1):xtv(end),fitline,fCIline,'g')
+                else
+                    hold on; shadedErrorBar(xtv(1):xtv(end),fitline,fCIline,'k')
+                end
+                
+                set(gca,'xlim',[min(xtv) max(xtv)],'ylim',[0 (max(featureMean{d}(selBins{d}).*1000)+.01).*1.2])
+                [~,idx] = max(fitline);
+                ranges = xtv(1):xtv(end);
+                peakResponse{i}(d) = ranges(idx);
+            end
         end
     end
     
     suptitle(fieldsToCompare{i})
     
-%    print(['C:\Users\jacheung\Dropbox\LocationCode\Figures\hilbertCode\Hilbert_whiskingTouch\' fieldsToCompare{i} '_whisking'],'-dpng')
-%    print(['C:\Users\jacheung\Dropbox\LocationCode\Figures\hilbertCode\Hilbert_whiskingTouch\' fieldsToCompare{i} '_whisking'],'-depsc')
+   print(['C:\Users\jacheung\Dropbox\LocationCode\Figures\hilbertCode\Hilbert_whiskingTouch\' fieldsToCompare{i} 'touch'],'-dpng')
+   print(['C:\Users\jacheung\Dropbox\LocationCode\Figures\hilbertCode\Hilbert_whiskingTouch\' fieldsToCompare{i} 'touch'],'-depsc')
 end
 
 
@@ -90,20 +114,6 @@ whichTouches = fields(popV{1});
 fieldsList = fields(popV{1}.allTouches);
 hilbertTouch = tuningQuantification(U,popV,selectedCells,fieldsList([ 1 3 4 5]),whichTouches,touchWindow,'off');
 
-touchFields = fields(hilbertTouch.R_ntk.allTouches);
-for d = 1:lenght(touchFields)
-    c_feat = hilbertTouch.R_ntk.allTouches.(touchFields{d});
-    TouchR = cellfun(@(x) nanmean(x,2),c_feat,'uniformoutput',0) * 1000;
-    responseSEM = cellfun(@(x) nanstd(x,[],2) ./ nansum(~isnan(x),2),c_feat,'uniformoutput',0) * 1000;
-    
-    for b = 1:length(c_feat)
-        c_cell=c_feat{b};
-        
-        if ~isempty(c_cell)
-        p_val{d}(b) = anova1(c_feat{b},[],'off');
-        
-        
-        
             
            
 
