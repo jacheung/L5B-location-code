e80a0e11a9828976b4c559fb1d488957414b1370
Jon Cheung <jacheung6@gmail.com>
2019-10-23 19:43:43 -0700

    up to date version of whisker position tuning

--- matlab/code/sandbox/whisking_position_tuning.m
+++ matlab/code/sandbox/whisking_position_tuning.m
@@ -1,4 +1,55 @@
 saveDir = 'C:\Users\jacheung\Dropbox\LocationCode\Figures\Parts\Fig4\';
+%% example whisker trace
+
+for k = 83
+    currentCell = U{k};
+    
+%      trialSample = [datasample(1:currentCell.k,5)];
+     trialSample = [134 136 datasample(1:currentCell.k,1)]; %CELL 83
+%         trialSample = [99 136 datasample(1:currentCell.k,3)]; %% CELL 22
+%         trialSample = [119 98 43 63 datasample(1:currentCell.k,1)]; %% CELL 33 EXAMPLE 119 SO GOOD!CELL TUNED TO ANGLE 10 . Dam, it's also phase tuned. LULz
+%         trialSample = [60 1 datasample(1:currentCell.k,1)]; %% CELL 74 maybe 96 too
+    figure(2310);clf
+    for g = 1:length(trialSample)
+        
+        whisk = currentCell.S_ctk(1,:,trialSample(g));
+        midpoint = currentCell.S_ctk(4,:,trialSample(g)); 
+        amp = currentCell.S_ctk(3,:,trialSample(g));
+        amp_whisk = whisk;
+        amp_whisk(amp<5) = nan;
+        touchesOn = [find(currentCell.S_ctk(9,:,trialSample(g))==1) find(currentCell.S_ctk(12,:,trialSample(g))==1)];
+        touchesOff = [find(currentCell.S_ctk(10,:,trialSample(g))==1) find(currentCell.S_ctk(13,:,trialSample(g))==1)];
+
+        if ~isempty(touchesOn)
+            for p = 1:numel(touchesOn)
+                amp_whisk(touchesOn(p):touchesOff(p)+30) = nan;
+            end
+        end
+        
+        spikes = find(currentCell.R_ntk(1,:,trialSample(g))==1);
+        subplot(length(trialSample),1,g)
+        plot(1:currentCell.t,whisk,'k')
+        hold on;plot(1:currentCell.t,amp_whisk,'c')
+        
+        hold on; scatter(spikes,whisk(spikes),'ko','filled','markerfacecolor',[.8 .8 .8])
+        hold on; scatter(spikes,amp_whisk(spikes),'ko','filled')
+
+        if ~isempty(touchesOn) && ~isempty(spikes) 
+            for f = 1:length(touchesOn)
+                tp = touchesOn(f):touchesOff(f);
+                hold on; plot(tp,whisk(tp),'r');
+            end
+        end
+        
+        title(['cell num ' num2str(k) ' at trial num ' num2str(trialSample(g))])
+        set(gca,'ylim',[-10 50])
+    end
+    
+end
+
+        fn = 'example_traces .eps';
+        export_fig([saveDir, fn], '-depsc', '-painters', '-r1200', '-transparent')
+        fix_eps_fonts([saveDir, fn])
 %% whisk x quiet
 masks = cellfun(@(x) maskBuilder(x),U,'uniformoutput',0);
 whisking_spks_mat = cellfun(@(x,y) squeeze(x.R_ntk).*y.whisking .*y.touch,U,masks,'uniformoutput',0);
@@ -73,14 +124,14 @@ prop_untuned = sum(non_tuned)./numel(tuned_units);
 pie([prop_tuned prop_untuned])
 
 subplot(4,2,[5 6]) 
-histogram(mod_idx_all_units,[0 .25 .5 1 2.5 5 10 25 50 100])
+histogram(mod_idx_all_units,[0.1 .25 .5 1 2.5 5 10 25 50 100])
 title('mod idx')
-set(gca,'xscale','log')
+set(gca,'xscale','log','xlim',[.1 100])
 
 subplot(4,2,[7 8])
-hold on; histogram(whisk_fr,[0 .25 .5 1 2.5 5 10 25 50 100])
+hold on; histogram(whisk_fr,[0.1 .25 .5 1 2.5 5 10 25 50 100])
 title('whisk fr')
-set(gca,'xscale','log')
+set(gca,'xscale','log','xlim',[.1 100])
 
 fn = 'fr_x_modulation_index.eps';
 export_fig([saveDir, fn], '-depsc', '-painters', '-r1200', '-transparent')
@@ -136,12 +187,10 @@ export_fig([saveDir, fn], '-depsc', '-painters', '-r1200', '-transparent')
 fix_eps_fonts([saveDir, fn])
 
 
-%% Proportion of units tuned 
+%% scatter of all whisk components (deep dive but not sure if necessary)
 
 
 
-    
-        saveDir = 'C:\Users\jacheung\Dropbox\LocationCode\Figures\Parts\Fig4\';
-        fn = 'whisker_position_indiv.eps';
-        export_fig([saveDir, fn], '-depsc', '-painters', '-r1200', '-transparent')
-        fix_eps_fonts([saveDir, fn])
+
+
+
