61f7fe20ca864ef4bc9e9ef9ccc847d5c2063a8a
Jon Cheung <jacheung6@gmail.com>
2019-11-06 14:22:03 -0800

    finalized sampling justification

--- matlab/code/dependentFunctions/sample_justification.m
+++ matlab/code/dependentFunctions/sample_justification.m
@@ -1,18 +1,20 @@
-function sample_justification(uberarray,selectedCells,hilbert_feature)
+function sample_justification(uberarray,hilbert_feature)
 
-selectedCells = 1:numel(uberarray) 
+disp('Iterating BARS fitting over cells and different bins. This may take a while...')
+
 whisk_num_bins = [200, 100, 50, 20, 10];
 touches_per_bin = [10 25 50 75 100 125 150 175 200];
 
-whisk_ol_p = cell(1,numel(selectedCells));
-touch_ol_p = cell(1,numel(selectedCells));
+whisk_ol_p = cell(1,numel(uberarray));
+touch_ol_p = cell(1,numel(uberarray));
 
 whisk = [];
-touch = []; 
+touch = [];
 
-for rec = 1:numel(selectedCells)
-    %% WHISKING
-    array = uberarray{selectedCells(rec)};
+for rec = 1:numel(uberarray)
+    disp([num2str(rec) '/' num2str(numel(uberarray))])
+    % WHISKING
+    array = uberarray{rec};
     spikes = squeeze(array.R_ntk);
     
     %whisking mask
@@ -50,7 +52,7 @@ for rec = 1:numel(selectedCells)
         nanmat = cell2nanmat(sorted);
         [whisk_ol_p{rec}(b),~,~] = anova1(nanmat,[],'off');
         
-        %BARS fitting
+%         BARS fitting
         x = cellfun(@median,sortedBy);
         y = cellfun(@mean, sorted);
         if strcmp(hilbert_feature,'phase') || strcmp(hilbert_feature,'pole')
@@ -77,79 +79,86 @@ for rec = 1:numel(selectedCells)
             whisk.tune_peak{rec}(b) = nan;
         end
         
-
+        
     end
     
-%     %% TOUCH
-%     %stimulus and response variables definitions
-%     touch_rw = array.meta.touchProperties.responseWindow;
-%     [tVar] = atTouch_sorter(array,touch_rw(1):touch_rw(2));
-%     
-%     if ~isempty(hilbert_feature)
-%         if strcmp(hilbert_feature,'angle')
-%             selected_feature = tVar.allTouches.S_ctk(:,1);
-%         elseif strcmp(hilbert_feature,'amplitude')
-%             selected_feature = tVar.allTouches.S_ctk(:,3);
-%         elseif strcmp(hilbert_feature,'midpoint')
-%             selected_feature = tVar.allTouches.S_ctk(:,4);
-%         elseif strcmp(hilbert_feature,'phase')
-%             selected_feature = tVar.allTouches.S_ctk(:,5);
-%         elseif strcmp(hilbert_feature,'curvature')
-%             selected_feature = tVar.allTouches.S_ctk(:,6);
-%         elseif strcmp(hilbert_feature,'pole')
-%             selected_feature = normalize_var(tVar.allTouches.S_ctk(:,7),-1,1);
-%         else
-%             error('select features of "angle", "amplitude", "midpoint", "phase", "curvature", or "pole"')
-%         end
-%     else
-%         error('select features of "angle", "amplitude", "midpoint", "phase", "curvature", or "pole"')
-%     end
-%     
-%     response = mean(tVar.allTouches.R_ntk,2) * 1000;
-%     
-%     for b = 1:numel(touches_per_bin)
-%         numBins = round(numel(selected_feature)./touches_per_bin(b));
-%         if numBins > 1
-%             [sorted_touch, sorted_by_touch] = binslin(selected_feature,response,'equalN',numBins);
-%             nanmat = cell2nanmat(sorted_touch);
-%             [touch_ol_p{rec}(b),~,~] = anova1(nanmat,[],'off');
-%             
-%             %BARS fitting
-%             x = cellfun(@median,sorted_by_touch);
-%             y = cellfun(@mean, sorted_touch);
-%             if strcmp(hilbert_feature,'phase') || strcmp(hilbert_feature,'pole')
-%                 xq = min(x):.1:max(x);
-%             else
-%                 xq = min(x):1:max(x);
-%             end
-%             yq = interp1(x,y,xq);
-%             
-%             barsFit = [];
-%             try
-%                 barsFit = barsP(yq,[min(xq) max(xq)],round(mean(cellfun(@numel,sorted))));
-%                 barsFit.x = xq;
-%                 smooth_response = barsFit.mean(2:end-1);
-%                 smooth_stimulus = xq(2:end-1);
-%                 [maxResponse,maxidx] = max(smooth_response);
-%                 [minResponse] = min(smooth_response);
-%                 
-%                 touch.mod_idx{rec}(b) = (maxResponse - minResponse) ./ (maxResponse + minResponse);
-%                 touch.tune_peak{rec}(b) = smooth_stimulus(maxidx);
-%             catch
-%                 disp(['skipping ' num2str(rec) ' due to ill fitting of bars'])
-%                 
-%                 touch.mod_idx{rec}(b) = nan;
-%                 touch.tune_peak{rec}(b) = nan;
-%             end
-%         else
-%             touch_ol_p{rec}(b) = nan;
-%         end
-%     end
+    %% TOUCH
+    %stimulus and response variables definitions
+    if isfield(array.meta,'touchProperties')
+        touch_rw = array.meta.touchProperties.responseWindow;
+        [tVar] = atTouch_sorter(array,touch_rw(1):touch_rw(2));
+        
+        if ~isempty(hilbert_feature)
+            if strcmp(hilbert_feature,'angle')
+                selected_feature = tVar.allTouches.S_ctk(:,1);
+            elseif strcmp(hilbert_feature,'amplitude')
+                selected_feature = tVar.allTouches.S_ctk(:,3);
+            elseif strcmp(hilbert_feature,'midpoint')
+                selected_feature = tVar.allTouches.S_ctk(:,4);
+            elseif strcmp(hilbert_feature,'phase')
+                selected_feature = tVar.allTouches.S_ctk(:,5);
+            elseif strcmp(hilbert_feature,'curvature')
+                selected_feature = tVar.allTouches.S_ctk(:,6);
+            elseif strcmp(hilbert_feature,'pole')
+                selected_feature = normalize_var(tVar.allTouches.S_ctk(:,7),-1,1);
+            else
+                error('select features of "angle", "amplitude", "midpoint", "phase", "curvature", or "pole"')
+            end
+        else
+            error('select features of "angle", "amplitude", "midpoint", "phase", "curvature", or "pole"')
+        end
+        
+        response = mean(tVar.allTouches.R_ntk,2) * 1000;
+        
+        for b = 1:numel(touches_per_bin)
+            numBins = round(numel(selected_feature)./touches_per_bin(b));
+            if numBins > 1
+                [sorted_touch, sorted_by_touch] = binslin(selected_feature,response,'equalN',numBins);
+                nanmat = cell2nanmat(sorted_touch);
+                [touch_ol_p{rec}(b),~,~] = anova1(nanmat,[],'off');
+                
+                %BARS fitting
+                x = cellfun(@median,sorted_by_touch);
+                y = cellfun(@mean, sorted_touch);
+                if strcmp(hilbert_feature,'phase') || strcmp(hilbert_feature,'pole')
+                    xq = min(x):.1:max(x);
+                else
+                    xq = min(x):1:max(x);
+                end
+                yq = interp1(x,y,xq);
+                
+                barsFit = [];
+                try
+                    barsFit = barsP(yq,[min(xq) max(xq)],round(mean(cellfun(@numel,sorted))));
+                    barsFit.x = xq;
+                    smooth_response = barsFit.mean(2:end-1);
+                    smooth_stimulus = xq(2:end-1);
+                    [maxResponse,maxidx] = max(smooth_response);
+                    [minResponse] = min(smooth_response);
+                    
+                    touch.mod_idx{rec}(b) = (maxResponse - minResponse) ./ (maxResponse + minResponse);
+                    touch.tune_peak{rec}(b) = smooth_stimulus(maxidx);
+                catch
+                    disp(['skipping ' num2str(rec) ' due to ill fitting of bars'])
+                    
+                    touch.mod_idx{rec}(b) = nan;
+                    touch.tune_peak{rec}(b) = nan;
+                end
+            else
+                touch_ol_p{rec}(b) = nan;
+            end
+            nanmat = nan(1,numel(touches_per_bin));
+            touch.mod_idx{rec} = nanmat;
+            touch.tune_peak{rec} = nanmat;
+            touch_ol_p{rec} = nanmat;
+            
+        end
+    end
     
 end
 
-%% plotting p-value 
-selected_whisk = cellfun(@(x) x(3),whisk_ol_p)<0.05; 
+%% plotting p-value
+selected_whisk = cellfun(@(x) x(3),whisk_ol_p)<0.05;
 figure(3840);clf
 
 subplot(1,2,1);
@@ -172,7 +181,7 @@ axis square
 ylabel('ANOVA p-value');
 xlabel('number of touches per bin')
 
-%% modulation index plotting
+% modulation index plotting
 mods_whisk = cell2mat(whisk.mod_idx(selected_whisk)')';
 figure(5180);clf
 subplot(2,2,1);
@@ -194,7 +203,7 @@ axis square
 ylabel('modulation depth');
 xlabel('number of touches per bin')
 
-%% modulation tune peak plotting
+% modulation tune peak plotting
 mean_norm_tune = cellfun(@(x) x-nanmean(x), whisk.tune_peak((selected_whisk)),'uniformoutput',0);
 whisk_peak = cell2mat(mean_norm_tune')';
 subplot(2,2,3);
