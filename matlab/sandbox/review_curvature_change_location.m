7a0b00aa9c50921d97a5c4b99c7e23d0fc3035ab
Jon Cheung <jacheung6@gmail.com>
2020-05-25 18:23:29 -0700

    addition of stratification analyses for reviewer 2

--- matlab/sandbox/review_curvature_change_location.m
+++ matlab/sandbox/review_curvature_change_location.m
@@ -7,8 +7,12 @@ dk_corr = zeros(1,numel(U));
 dt_corr = zeros(1,numel(U));
 fitCoeffs = cell(1,numel(U));
 fitDevExplained = zeros(1,numel(U));
+rsq = zeros(1,numel(U));
 
-for i = 1:length(U)
+num_curvature_bins = 3; 
+num_angle_bins = 9;
+
+for i = 29
     
     % raw variables
     touchOn = [find(U{i}.S_ctk(9,:,:)==1)  ;find(U{i}.S_ctk(12,:,:)==1)];
@@ -37,89 +41,111 @@ for i = 1:length(U)
     
     % touch response
     try
+        % response calculation
         response_window = U{i}.meta.touchProperties.responseWindow(1):U{i}.meta.touchProperties.responseWindow(2);
+        tresponse = mean(spikes(touchOn + response_window),2) * 1000;
+    
+        % plotting
+        % protraction dk/dt correlation w/ pole location
+        pt = dk<0;
+        dk_corr(i) = corr(touch_poles(pt)',dk(pt));
+        dt_corr(i) = corr(touch_poles(pt)',dt(pt));
+
+        pt_spikes = tresponse(pt);
+        pt_dk = dk(pt);
+        pt_theta = touchTheta(pt);
+
+        norm_pt = normalize_var(pt_spikes,1,0.1);
+        [sort_colors,sort_idx] = sort(norm_pt,'descend');
+        figure(03);clf
+        subplot(2,1,1)
+        scatter(pt_theta(sort_idx),pt_dk(sort_idx),20,repmat(sort_colors,1,3),'filled');
+        xlabel('whisker angle at touch')
+        ylabel('max curvature')
+        title('protraction touch analyesis')
+        %stratification analyses (low and high curvature
+        
+        [dk_sorted, dksby] = binslin(pt_dk,[pt_theta pt_spikes],'equalN',num_curvature_bins);
+        
+        subplot(2,1,2)
+        for p = 1:numel(dksby)
+            [sorted, sortedby] = binslin(dk_sorted{p}(:,1),dk_sorted{p}(:,2),'equalN',num_angle_bins);     
+            hold on; plot(cellfun(@mean, sortedby),cellfun(@mean,sorted))
+        end
+        xlabel('whisker angle at touch')
+        ylabel('firing rate (spks/s)')
+        legend('high dk','mid dk','low dk')
+        
+        if mean( cellfun(@numel,sorted)) < 20
+            disp('warning: number of samples per angle bin < 20')
+        end
+        
     catch
         touch_cells = cellfun(@(x) isfield(x.meta.touchProperties,'responseWindow'),U);
         rw = mean(cell2mat(cellfun(@(x) x.meta.touchProperties.responseWindow,U(touch_cells),'uniformoutput',0)'));
         response_window = round(rw(1):rw(2));
+        
+        disp(['cell ' num2str(i) ' is not a touch cell. Skipping']) 
     end
     
-    tresponse = sum(spikes(touchOn + response_window),2);
     
-    %     figure(230);clf
-    %     subplot(1,2,1);
-    %     scatter(touch_poles,dk,'k.')
-    %     hold on; plot([0 10],[0 0],'--k')
-    %     title('max dk during touch')
-    %     xlabel('normalized pole location')
-    %     ylabel('max dk')
-    %
-    %     subplot(1,2,2);
-    %     scatter(touch_poles,dt,'k.')
-    %     hold on; plot([0 10],[0 0],'--k')
-    %     title('max dtheta during touch')
-    %     xlabel('normalized pole location')
-    %     ylabel('max dt')
-    
-    % protraction dk/dt correlation w/ pole location
-    pt = dk<0;
-    dk_corr(i) = corr(touch_poles(pt)',dk(pt));
-    dt_corr(i) = corr(touch_poles(pt)',dt(pt));
-    
-    pt_spikes = tresponse(pt);
-    pt_dk = dk(pt);
-    pt_theta = touchTheta(pt);
-    
-    norm_pt = normalize_var(pt_spikes,1,0);
-    %     figure(03);clf
-    %     scatter(pt_theta,pt_dk,20,repmat(norm_pt,1,3),'filled');
     %% simple glm for touch cells only
-    if isfield(U{i}.meta.touchProperties,'responseWindow')
-        
-        glmnetOpt = glmnetSet;
-        glmnetOpt.standardize = 0;
-        glmnetOpt.alpha = 0.95;
-        glmnetOpt.xfoldCV = 5;
-        glmnetOpt.numIterations = 25;
-        
-        DmatX = [pt_dk pt_theta];
-        norm_DmatX = (DmatX - mean(DmatX)) ./ std(DmatX);
-        DmatY = pt_spikes;
-        
-        % test and train split
-        exampleIdx = 1:length(pt_spikes);
-        shuffIdx = exampleIdx(randperm(length(exampleIdx)));
-        trainIdx = shuffIdx(1:round(length(pt_spikes)*.8));
-        testIdx = setdiff(shuffIdx,trainIdx);
-        
-        trainDmatX = norm_DmatX(trainIdx',:);
-        trainDmatY = DmatY(trainIdx',:);
-        testDmatX = norm_DmatX(testIdx',:);
-        testDmatY = DmatY(testIdx',:);
-        
-        % model fitting
-        try
-            cv = cvglmnet(trainDmatX,trainDmatY,'poisson',glmnetOpt,[],glmnetOpt.xfoldCV);
-            
-            % model evaluation
-            fitLambda = cv.lambda_1se;
-            iLambda = find(cv.lambda == fitLambda);
-            fitCoeffs{i} = [cv.glmnet_fit.a0(iLambda) ; cv.glmnet_fit.beta(:,iLambda)];
-            
-            model = exp([ones(length(testDmatX),1),testDmatX]*fitCoeffs{i});
-            mu = mean(testDmatY); % null poisson parameter
-            nullLogLikelihood = sum(log(poisspdf(testDmatY,mu)));
-            saturatedLogLikelihood = sum(log(poisspdf(testDmatY,testDmatY)));
-            fullLogLikelihood = sum(log(poisspdf(testDmatY,model)));
-            fitDevExplained(i) = (fullLogLikelihood - nullLogLikelihood)/(saturatedLogLikelihood - nullLogLikelihood);
-        catch
-            fitDevExplained(i) = nan;
-            fitCoeffs{i} = nan(size(DmatX,2)+1,1);
-        end
-    else
-        fitDevExplained(i) = nan;
-        fitCoeffs{i} = nan(size(DmatX,2)+1,1);
-    end
+    % as of 200522, model is garbage at predicting spikes using poisson and
+    % dk and theta at touch for each touch. Response probabilites are just
+    % way too low. Model using mean predicts FR suggesting that features
+    % arent' very useful in driving neural activity over touch response
+    % window. 
+%     if isfield(U{i}.meta.touchProperties,'responseWindow')
+%         
+%         glmnetOpt = glmnetSet;
+%         glmnetOpt.standardize = 0;
+%         glmnetOpt.alpha = 0.95;
+%         glmnetOpt.xfoldCV = 5;
+%         glmnetOpt.numIterations = 25;
+%         
+%         DmatX = [pt_dk pt_theta];
+%         norm_DmatX = (DmatX - mean(DmatX)) ./ std(DmatX);
+%         DmatY = pt_spikes;
+%         
+%         % test and train split
+%         exampleIdx = 1:length(pt_spikes);
+%         shuffIdx = exampleIdx(randperm(length(exampleIdx)));
+%         trainIdx = shuffIdx(1:round(length(pt_spikes)*.8));
+%         testIdx = setdiff(shuffIdx,trainIdx);
+%         
+%         trainDmatX = norm_DmatX(trainIdx',:);
+%         trainDmatY = DmatY(trainIdx',:);
+%         testDmatX = norm_DmatX(testIdx',:);
+%         testDmatY = DmatY(testIdx',:);
+%         
+%         % model fitting
+%         try
+%             cv = cvglmnet(trainDmatX,trainDmatY,'poisson',glmnetOpt,[],glmnetOpt.xfoldCV);
+%             
+%             % model evaluation
+%             fitLambda = cv.lambda_1se;
+%             iLambda = find(cv.lambda == fitLambda);
+%             fitCoeffs{i} = [cv.glmnet_fit.a0(iLambda) ; cv.glmnet_fit.beta(:,iLambda)];
+%             
+%             model = exp([ones(length(testDmatX),1),testDmatX]*fitCoeffs{i});
+%             mu = mean(testDmatY); % null poisson parameter
+%             nullLogLikelihood = sum(log(poisspdf(testDmatY,mu)));
+%             saturatedLogLikelihood = sum(log(poisspdf(testDmatY,testDmatY)));
+%             fullLogLikelihood = sum(log(poisspdf(testDmatY,model)));
+%             fitDevExplained(i) = (fullLogLikelihood - nullLogLikelihood)/(saturatedLogLikelihood - nullLogLikelihood);
+%             MSE = mean((testDmatY - model).^2);
+%             MSE_bl = mean((testDmatY-mu).^2);
+%             rsq(i) = 1 - (MSE/MSE_bl);
+%         catch
+%             fitDevExplained(i) = nan;
+%             fitCoeffs{i} = nan(size(DmatX,2)+1,1);
+%             rsq(i) = nan;
+%         end
+%     else
+%         fitDevExplained(i) = nan;
+%         fitCoeffs{i} = nan(size(DmatX,2)+1,1);
+%         rsq(i) = nan;
+%     end
 end
 
 
